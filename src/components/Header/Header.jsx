d5ca21440008bf1d6fe6f5ff340d98c63a0baeb0
erickmarquesc <erick.marquesc@hotmail.com>
2022-11-07 18:32:25 -0300

    feat: :zap: create Header components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../../assets/Logo.svg';
+import '../../styles/Header.scss';
+
+export function Header() {
+  return (
+
+    <div>
+      <img src={logo}/>
+    </div>
+
+  );
+};
